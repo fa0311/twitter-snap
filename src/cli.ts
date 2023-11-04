9ed2237b3614411cb0d644f2af98b8cbd011183a
ふぁ <yuki@yuki0311.com>
2023-11-05 07:44:04 +0900

    update @vercel/og
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 import { TwitterSnap } from "./core/twitterSnap";
 import { Command } from "commander";
-import { promises as fs, realpathSync } from "fs";
-import * as path from "path";
-
-const current = path.dirname(realpathSync(__filename));
+import { promises as fs } from "fs";
 
 const version = "0.0.8";
-const font = path.join(current, "./assets/NotoSansCJKjp-Regular.otf");
-const boldFont = path.join(current, "./assets/NotoSansCJKjp-Bold.otf");
-const emojiFont = path.join(current, "./assets/twemoji/{code}.svg");
 
 const program = new Command();
 program.name("twitter-snap").version(version);
@@ -20,32 +14,16 @@ program
   .option("-w, --width <number>", "image width", "600")
   .option("-h, --height <number>", "image height")
   .option("-s, --style <string>", "style", "normal")
-  .option("--font <path>", "font path", font)
-  .option("--bold-font <path>", "bold font path", boldFont)
-  .option("--emoji-font <path>", "emoji font path", emojiFont)
-  .action(async (text, { output, width, height, style, font, boldFont }) => {
+  .action(async (text, { output, width, height, style }) => {
     const id = isNaN(text) ? text.split("/").pop() : text;
     const twitterSnap = new TwitterSnap({
       width: parseInt(width),
       height: height ? parseInt(height) : undefined,
-      font: [
-        {
-          name: "Regular",
-          data: await fs.readFile(font),
-          weight: 400,
-          style: "normal",
-        },
-        {
-          name: "Bold",
-          data: await fs.readFile(boldFont),
-          weight: 700,
-          style: "normal",
-        },
-      ],
-      emojiFont: emojiFont,
     });
-    const writer = await twitterSnap.render({ id, styleName: style });
-    await writer.save(output);
+    const res = await twitterSnap.render({ id, styleName: style });
+    const png = Buffer.from(await res.arrayBuffer());
+    await fs.writeFile(output, png);
+
     process.exit(0);
   });
 program.parse(process.argv);
--- src/core/twitterSnap.ts
+++ src/core/twitterSnap.ts
@@ -1,8 +1,7 @@
-import satori, { SatoriOptions } from "satori";
 import { Transformer } from "@napi-rs/image";
 import Normal from "./../style/normal";
 import { promises as fs } from "fs";
-import { FontOptions } from "./satori";
+import { ImageResponse } from "@vercel/og";
 import { TwitterOpenApi, TweetApiUtilsData } from "twitter-openapi-typescript";
 
 export type StyleComponent = (props: {
@@ -12,8 +11,6 @@ export type StyleComponent = (props: {
 type TwitterSnapParams = {
   width: number;
   height?: number;
-  font: FontOptions[];
-  emojiFont: string;
 };
 type TwitterSnapRenderParams = {
   id: string;
@@ -23,7 +20,6 @@ type TwitterSnapRenderParams = {
 export class TwitterSnap {
   width!: number;
   height!: number | undefined;
-  option!: SatoriOptions;
 
   static styles: { [key: string]: StyleComponent } = {
     normal: Normal,
@@ -32,25 +28,6 @@ export class TwitterSnap {
   constructor(param: TwitterSnapParams) {
     this.width = param.width;
     this.height = param.height;
-
-    this.option = {
-      width: this.width,
-      height: this.height,
-      fonts: param.font,
-      loadAdditionalAsset: async (code: string, segment: string) => {
-        if (code === "emoji") {
-          const base64Header = "data:image/svg+xml;base64,";
-          const unicode = Array.from(segment)
-            .map((c) => c.codePointAt(0)!.toString(16))
-            .join("-");
-          const res = await fs.readFile(
-            param.emojiFont.replace("{code}", unicode)
-          );
-          return `${base64Header}${res.toString("base64")}`;
-        }
-        return "";
-      },
-    };
   }
 
   render = async ({ id, styleName }: TwitterSnapRenderParams) => {
@@ -59,118 +36,10 @@ export class TwitterSnap {
       tweetId: id,
     });
     const style = TwitterSnap.styles[styleName || "normal"];
-    const svg = await satori(style({ data: tweet.data! }), this.option);
-    return new TwitterSnapWriter({
+    const res = new ImageResponse(style({ data: tweet.data! }), {
       width: this.width,
       height: this.height,
-      svg: svg,
     });
-  };
-}
-
-type TwitterSnapWriterParams = {
-  width: number;
-  height: number | undefined;
-  svg: string;
-};
-
-export class TwitterSnapWriter {
-  data!: string;
-  _trasformer: Transformer | undefined;
-
-  constructor(param: TwitterSnapWriterParams) {
-    this.data = param.svg;
-  }
-
-  trasformer = async () => {
-    if (!this._trasformer) {
-      this._trasformer = Transformer.fromSvg(this.data);
-      const re = new RegExp(/viewBox="(\d+) (\d+) (\d+) (\d+)"/);
-      const [x, y, width, height] = this.data.match(re)!.slice(1).map(Number);
-      this._trasformer.crop(x, y, width, height);
-    }
-    return this._trasformer;
-  };
-
-  save = async (output: string) => {
-    const ext = output.split(".").pop();
-    const exts: {
-      [key: string]: (output: string) => Promise<void>;
-    } = {
-      raw: this.raw,
-      svg: this.svg,
-      png: this.png,
-      jpeg: this.jpeg,
-      jpg: this.jpeg,
-      jpe: this.jpeg,
-      bmp: this.bmp,
-      dib: this.bmp,
-      rle: this.bmp,
-      "2bp": this.bmp,
-      "2pbb": this.bmp,
-      ico: this.ico,
-      cur: this.ico,
-      tif: this.tiff,
-      webp: this.webp,
-      avif: this.avif,
-      pnm: this.pnm,
-      tga: this.tga,
-      tpic: this.tga,
-      ff: this.farbfeld,
-    };
-    const method = exts[ext!];
-    if (!method) {
-      throw new Error("Unsupported file format");
-    }
-    await method(output);
-  };
-
-  svg = async (output: string) => {
-    await fs.writeFile(output, this.data);
-  };
-  raw = async (output: string) => {
-    await fs.writeFile(output, this.data);
-  };
-  jpeg = async (output: string) => {
-    const data = await (await this.trasformer()).jpeg();
-    await fs.writeFile(output, data);
-  };
-  png = async (output: string) => {
-    const data = await (await this.trasformer()).png();
-    await fs.writeFile(output, data);
-  };
-
-  bmp = async (output: string) => {
-    const data = await (await this.trasformer()).bmp();
-    await fs.writeFile(output, data);
-  };
-
-  ico = async (output: string) => {
-    const data = await (await this.trasformer()).ico();
-    await fs.writeFile(output, data);
-  };
-  tiff = async (output: string) => {
-    const data = await (await this.trasformer()).tiff();
-    await fs.writeFile(output, data);
-  };
-  webp = async (output: string) => {
-    const data = await (await this.trasformer()).webp();
-    await fs.writeFile(output, data);
-  };
-  avif = async (output: string) => {
-    const data = await (await this.trasformer()).avif();
-    await fs.writeFile(output, data);
-  };
-  pnm = async (output: string) => {
-    const data = await (await this.trasformer()).pnm();
-    await fs.writeFile(output, data);
-  };
-  tga = async (output: string) => {
-    const data = await (await this.trasformer()).tga();
-    await fs.writeFile(output, data);
-  };
-  farbfeld = async (output: string) => {
-    const data = await (await this.trasformer()).farbfeld();
-    await fs.writeFile(output, data);
+    return res;
   };
 }
