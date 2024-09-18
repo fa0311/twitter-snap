7d913b8add73876d489c9f2d4d2ba9a562f40ebb
ふぁ <yuki@yuki0311.com>
2024-09-18 21:59:37 +0900

    add export
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import Default from './commands'
 
+export {getFonts, twitterSnapCookies, twitterSnapGuest, twitterSnapPuppeteer} from './core/core'
+export {TwitterSnap} from './core/main'
+
 export default class TwitterSnap extends Default {}
